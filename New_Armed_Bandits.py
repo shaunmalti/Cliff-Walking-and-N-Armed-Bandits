1320cf32580bf458e64b9eca6e008798ca20de9c
Shaun Markham <shaunmarkham30@gmail.com>
2018-03-08 20:36:00 +0000

    added comments

--- New_Armed_Bandits.py
+++ New_Armed_Bandits.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 
 def get_reward(action, arm_values):
+    # retrieve reward from reward array
     val1 = np.random.normal(0,1)
     reward = arm_values[0][action] + val1
     best_reward = np.argmax(arm_values[0])
@@ -10,16 +11,20 @@ def get_reward(action, arm_values):
 
 
 def update_est(action,reward, K, est_values):
+    # update all values, together with estimation and pick count
     K[0][action] += 1
     alpha = 1. / K[0][action]
-    est_values[0][action] += alpha * (reward - est_values[0][action])  # keeps running average of rewards
+    est_values[0][action] += alpha * (reward - est_values[0][action])
 
 
 def choose_eps_greedy(epsilon, est_values):
     rand_num = np.random.random()
     if rand_num > 1-epsilon:
+        # if random number is greater than 1-epsilon pick random
+        # i.e. > 0.9 or 10%
         return np.random.randint(10)
     else:
+        # choose the best action
         return np.argmax(est_values)
 
 
@@ -30,33 +35,50 @@ def choose_greedy(est_values):
 def experiment(arm_vals, K, est_values, Npulls,epsilon, option, avg_outcome_eps0p01):
     history = []
     reward = []
+    # for the number of defined pulls
     for i in range(Npulls):
+        # choose action method depending on function call
         if option == 1:
             action = choose_eps_greedy(epsilon, est_values)
         else:
             action = choose_greedy(est_values)
+
+        # get reward from carried out action
         R, best_reward = get_reward(action, arm_vals)
+
+        # this is done to check if action picked is optimal and then chart % optimal pick
         if action==best_reward:
             reward.append(1)
         else:
             reward.append(0)
+
+        # update agent estimations
         update_est(action, R, K, est_values)
+
+        # update reward gained array
         history.append(R)
+    # add history array to carry out average at the end
     avg_outcome_eps0p01 += np.array(history)
-    # return np.array(history)
     return np.array(reward)
 
 
 def main():
-    Nexp = 200
+    # initialisation of number of episodes and number of pulls per episode
+    Nexp = 2000
     Npulls = 1000
+
+    # initialisation of arrays
     avg_outcome_eps0p01 = np.zeros(Npulls)
     avg_outcome_eps0p1 = np.zeros(Npulls)
     avg_outcome_eps_greedy = np.zeros(Npulls)
     avg_perc_eps0p01 = np.zeros(Npulls)
     avg_perc_eps0p1 = np.zeros(Npulls)
     avg_perc_eps_greedy = np.zeros(Npulls)
+
+    # for number of episodes
     for i in range(Nexp):
+        # set array values and start experiment for different algorithms
+        # and different values of epsilon
         arm_vals = np.array([[np.random.normal(0, 1, 10)]])
         K = np.array([[np.zeros(10)]])
         est_values = np.array([[np.zeros(10)]])
@@ -72,18 +94,20 @@ def main():
         est_values = np.array([[np.zeros(10)]])
         avg_perc_eps_greedy += experiment(arm_vals[0], K[0], est_values[0], Npulls, 0, 2, avg_outcome_eps_greedy)
 
+    # take average for rewards
     avg_outcome_eps0p01 /= np.float(Nexp)
     avg_outcome_eps0p1 /= np.float(Nexp)
     avg_outcome_eps_greedy /= np.float(Nexp)
 
+    # take average for % best pick
     avg_perc_eps0p01 /= np.float(Nexp)
     avg_perc_eps0p1 /= np.float(Nexp)
     avg_perc_eps_greedy /= np.float(Nexp)
 
+    # plots
     plt.plot(avg_outcome_eps0p01, label="eps = 0.01")
     plt.plot(avg_outcome_eps0p1, label="eps = 0.1")
     plt.plot(avg_outcome_eps_greedy, label="greedy")
-
 
     plt.ylim(0, 2.2)
     plt.xlabel("Steps")
--- New_Cliff_Walking.py
+++ New_Cliff_Walking.py
@@ -4,15 +4,21 @@ import matplotlib.pyplot as plt
 
 class Mouse():
     def __init__(self):
+        # initialise agent with variables
         self.pos = [3,0]
         self.actionvalues = np.zeros(shape=(4,12,4),dtype = float)
 
 
 def all_possible_moves():
+    # define all possible movements
     actionDestination = []
+    # iterate through each possible row
     for i in range(0, 4):
         actionDestination.append([])
+        # iterate through each possible column
         for j in range(0, 12):
+            # create dictionary for each position that contains
+            # all possible moves with respect to that position
             destination = dict()
             destination[0] = [max(i - 1, 0), j]
             destination[2] = [i, max(j - 1, 0)]
@@ -28,30 +34,46 @@ def all_possible_moves():
 
 def Qlearning(mouse, epsilon, alpha, gamma, destination, ending_pos, rewards):
     tot_reward = 0.0
+    # while mouse has not reached the terminal position
     while mouse.pos != ending_pos:
+        # choose action
         action = choose_action_greedy(epsilon, mouse, mouse.pos)
+        # obtain reward
         reward = rewards[mouse.pos[0]][mouse.pos[1]][action]
+        # update total reward
         tot_reward += reward
+        # obtain new destination
         new_pos = destination[mouse.pos[0]][mouse.pos[1]][action]
+        # update using Qlearning update rule
         mouse.actionvalues[mouse.pos[0], mouse.pos[1], action] += alpha * (
             reward + gamma * np.max(mouse.actionvalues[new_pos[0], new_pos[1], :]) -
             mouse.actionvalues[mouse.pos[0], mouse.pos[1], action])
+        # set new position
         mouse.pos = new_pos
     return tot_reward
 
 
 def SARSAlearning(mouse, epsilon, alpha, gamma, destination, ending_pos, rewards):
+    # choose an action
     action_curr = choose_action_greedy(epsilon, mouse, mouse.pos)
     tot_reward = 0
+    # while mouse has not reached the terminal position
     while mouse.pos != ending_pos:
+        # define to 'would be' new state
         state_new = destination[mouse.pos[0]][mouse.pos[1]][action_curr]
+        # find out the new action from this new state
         action_new = choose_action_greedy(epsilon, mouse, state_new)
+        # get the reward for the old action
         reward = rewards[mouse.pos[0]][mouse.pos[1]][action_curr]
+        # update total reward value
         tot_reward += reward
+        # update using SARSA update rule
         mouse.actionvalues[mouse.pos[0]][mouse.pos[1]][action_curr] += alpha * (reward +
                 (gamma * mouse.actionvalues[state_new[0]][state_new[1]][action_new])
                 - mouse.actionvalues[mouse.pos[0]][mouse.pos[1]][action_curr])
+        # set the current action as the new action
         action_curr = action_new
+        # set the agent position as the new state
         mouse.pos = state_new
     return tot_reward
 
@@ -59,41 +81,53 @@ def SARSAlearning(mouse, epsilon, alpha, gamma, destination, ending_pos, rewards
 def choose_action_greedy(epsilon, mouse, state):
     rand_num = np.random.random()
     if rand_num > 1 - epsilon:
+        # if random number is greater than 1-epsilon pick random move
+        # example 1-0.1 = 0.9 random must be greater than 0.9
         return np.random.randint(4)
     else:
+        # obtain all action values from estimation matrix
         action_vals = mouse.actionvalues[state[0]][state[1]][:]
+        # choose the action based on maximum action value
+        # if more than one have the same action value then pick random
         action = np.random.choice(np.argwhere(action_vals == np.amax(action_vals)).flatten().tolist())
         return action
 
 def main():
+    # define environment with rewards
     actionRewards = np.zeros(shape=(4, 12, 4),dtype=float)
     actionRewards[:, :, :] = -1.0
     actionRewards[2, 1:11, 1] = -100.0
     actionRewards[3, 0, 3] = -100.0
 
+    # define all other constants
     ending_pos = [3,11]
     alpha = 0.1
     gamma = 1
     numSteps = 500
     numRuns = 10
-
     small_epsi = 0.01
     smallest_epsi = 0.0001
 
+    # produce all possible moves and save in array of dictionaries
     destination = all_possible_moves()
 
+    # create agents
     mouse_q = Mouse()
     mouse_sarsa = Mouse()
     mouse_s = Mouse()
     mouse_ss = Mouse()
 
+    # create arrays
     reward_q = np.zeros(numSteps)
     reward_sarsa = np.zeros(numSteps)
     reward_sarsa_s = np.zeros(numSteps)
     reward_sarsa_ss = np.zeros(numSteps)
 
+    # for number of runs
     for z in range(0,numRuns):
+        # for number of steps
         for i in range(0, numSteps):
+            # do learning algorithms, each being carried out until terminal state has been reached
             tot_reward_q = Qlearning(mouse_q, 0.1, alpha, gamma, destination, ending_pos, actionRewards)
             reward_q[i] += tot_reward_q
             mouse_q.pos = [3,0]
@@ -112,6 +146,7 @@ def main():
             reward_sarsa_ss[i] += tot_reward_sarsa_ss
             mouse_ss.pos = [3, 0]
 
+    # average over 10 runs
     reward_q /= numRuns
     reward_sarsa /= numRuns
     reward_sarsa_s /= numRuns
@@ -122,6 +157,7 @@ def main():
     smooth_rss = np.copy(reward_sarsa_s)
     smooth_rsss = np.copy(reward_sarsa_ss)
 
+    # perform moving average over last 19 episodes
     for i in range(numRuns, numSteps):
         smooth_rs[i] = np.mean(reward_sarsa[i - numRuns: i + 1])
         smooth_rq[i] = np.mean(reward_q[i - numRuns: i + 1])
